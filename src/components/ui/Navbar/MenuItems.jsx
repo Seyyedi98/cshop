451f3e6dba680a049effa4f26a021b495b05ee2e
Mreza <seyyedi98@outlook.com>
2024-01-18 21:12:34 +0330

    Fixed messup :(

--- src/components/ui/Navbar/MenuItems.jsx
+++ src/components/ui/Navbar/MenuItems.jsx
@@ -3,11 +3,7 @@ import MenuItem from "./MenuItem";
 function MenuItems() {
   return (
     <div className="fixed grid h-screen w-screen place-items-center md:relative md:h-full md:w-full">
-<<<<<<< HEAD
       <ul className="flex flex-col justify-between gap-3 md:flex-row lg:gap-5 ">
-=======
-      <ul className="flex flex-col justify-between gap-3 md:flex-row lg:gap-5">
->>>>>>> 3076b42fc70b1db2c088781b9eb7acfd31d1c123
         <MenuItem title="Home" />
         <MenuItem title="Shop" />
         <MenuItem title="Categories" dropdown={true} />
